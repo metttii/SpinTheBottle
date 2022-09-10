d42ee4af8cc6318f8401cb7cb677494af8b2738e
Metti <gelico@hotmail.de>
2022-09-10 18:14:23 +0200

    Further CSS improvements and restructuration

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var sectionids = [];
 var angles = [];
 var playerbtn = document.querySelector("#nplayer_button");
 var resetbtn = document.getElementById("reset_button");
-var arrow = document.getElementById("arrow_png")
 var savebtn = document.createElement('button');
 var startbtn = document.createElement('button');
-var spinbtn = document.createElement('button');
+//var spinbtn = document.createElement('button');
 var spinner = document.createElement('img');
 
 
@@ -26,26 +25,26 @@ function delay(milliseconds){
 //Get Number of Players and create NameInputFields
 
 playerbtn.addEventListener("click", function(){
-    nplayers = document.getElementById("nplayers").value;
-    startsection = document.getElementById("start_section");
-    startsection.innerHTML = '';
+    var nplayers = document.getElementById("nplayers").value;
+    section = document.getElementById("start_section");
+    section.innerHTML = '';
     for (let i = 0; i < nplayers; i++) {
         let inputfield = document.createElement("input");
-        let section = document.createElement("div");
-        section.id = `sec_player_${i+1}`;
+        let playersection = document.createElement("div");
+        playersection.id = `sec_player_${i+1}`;
         inputfield.id = `inp_player_${i+1}`;
         inputfield.type = 'text';
         inputfield.className = "inputfield";
         inputfield.placeholder = `Player ${i+1} name`;
         playerfieldids.push(inputfield.id);
-        sectionids.push(section.id);
-        document.body.appendChild(section);
-        section.appendChild(inputfield);
+        sectionids.push(playersection.id);
+        section.appendChild(playersection);
+        playersection.appendChild(inputfield);
     };
     savebtn.id = "saveplayer_button";
     savebtn.textContent = "Save Players";
-    savebtn.className = "inputfield";
-    document.body.appendChild(savebtn);
+    savebtn.className = "button";
+    section.appendChild(savebtn);
 });
 
 //Get player names and save them in an array
@@ -56,7 +55,7 @@ savebtn.addEventListener("click", function(){
         playerfieldelement = document.getElementById(playerfieldids[i]);
         sectionparent = document.getElementById(sectionids[i]);
         players.push(playername);
-        let newItem = document.createElement('h');
+        let newItem = document.createElement('div');
         newItem.innerHTML = playername;
         newItem.className = 'playername'
         sectionparent.replaceChild(newItem, playerfieldelement);
@@ -66,7 +65,7 @@ startbtn.id = "start_button";
 startbtn.textContent = "Start Game";
 startbtn.className = "button";
 oldbtn = document.getElementById("saveplayer_button");
-document.body.replaceChild(startbtn, oldbtn);
+section.replaceChild(startbtn, oldbtn);
 });
 
 //Create circle and place names around circle
@@ -88,7 +87,7 @@ var Position = {
     ellipse : function(n, rx, ry, so, wh, idd, cls, cw) {
       var m = document.createElement('div'), ss = document.styleSheets;
       ss[0].insertRule('#' + idd + ' { position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); border-radius: 50%; width: ' + String((rx * 2) + wh) + 'px; height: ' + String((ry * 2) + wh) + 'px; }', 1);
-      ss[0].insertRule('.' + cls + '{ position: absolute; background: rgba(0, 0, 0, 0.2); color: papayawhip; text-align: center; transition: transform 0.2s ease; width: fit-content; height: fit-content; border: 3px solid #282828; border-radius: 5px; box-shadow: -2px -2px 5px rgba(67,67,67,0.5), inset 2px 2px 5px rgba(0,0,0,0.5), inset -2px -2px 5px rgba(67,67,67,0.5), 2px 2px 5px rgba(0,0,0,0.3);  padding: 1% 2% 1% 2%; display: inline-block}', 1);
+      ss[0].insertRule('.' + cls + '{ position: absolute; background: rgba(0, 0, 0, 0.2); color: #ffffff; text-align: center; transition: transform 0.2s ease; width: fit-content; height: fit-content; border: 3px solid #282828; border-radius: 5px; box-shadow: -2px -2px 5px rgba(67,67,67,0.5), inset 2px 2px 5px rgba(0,0,0,0.5), inset -2px -2px 5px rgba(67,67,67,0.5), 2px 2px 5px rgba(0,0,0,0.3);  padding: 1% 2% 1% 2%; display: inline-block}', 1);
       m.id = idd;
       for (var i = 0, max = players.length; i < max; i++) {
         var c = document.createElement('div');
@@ -107,32 +106,34 @@ var Position = {
     }
   };
 
-var spinfield = [];
+//Load Main Game
 
 startbtn.addEventListener("click", function(){
     document.body.innerHTML = '';
     let circle = document.createElement('div');
     circle.className = "circle";
+    circle.id = "circle"
     document.body.appendChild(circle);
     Position.ellipse(players.length, 200, 200, 0, 35, 'main', 'node', true);
-    spinbtn.id = "spin_button";
-    spinbtn.textContent = "Spin";
-    document.body.appendChild(spinbtn);
+    //spinbtn.id = "spin_button";
+    //spinbtn.textContent = "Spin";
+    //document.body.appendChild(spinbtn);
     spinner.src = "arrow.png";
     spinner.className = "spinner";
     spinner.id = "arrow_png";
     spinner.alt = "";
     circle.appendChild(spinner);
-    console.log(spinfield);
-
+    var arrow = document.getElementById("circle");
+    console.log(arrow);
 });
 
-$(document).on('keypress', function(e){
-    if(e.which == 13) {
-        console.log(angles.length);
-        var finalrandomdeg = Math.floor(Math.random() * angles.length);
-        var ranresult = 720 * Math.floor(Math.random() * 20);
-        var target = angles[finalrandomdeg] - 90 + ranresult;
-        document.getElementById("arrow_png").style.transform = "rotate(" + target + "deg)";
-    }
-});
+//Spin on enter
+
+
+arrow.onclick = function(){
+    console.log(angles.length);
+    var finalrandomdeg = Math.floor(Math.random() * angles.length);
+    var ranresult = 720 * Math.floor(Math.random() * 20);
+    var target = angles[finalrandomdeg] - 90 + ranresult;
+    document.getElementById("arrow_png").style.transform = "rotate(" + target + "deg)";
+};
