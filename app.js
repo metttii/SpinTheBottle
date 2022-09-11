919f345339b081b6c46be86aacc8f3bde24dc66e
metttii <gelico@hotmail.de>
2022-09-11 13:57:05 +0200

    Version 0.2 - Major design improvements

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var playerfieldids = [];
 var sectionids = [];
 var angles = [];
 var playerbtn = document.querySelector("#nplayer_button");
-var resetbtn = document.getElementById("reset_button");
 var savebtn = document.createElement('button');
 var startbtn = document.createElement('button');
 var spinner = document.createElement('img');
 var challengewindow = document.createElement('div')
 var closechallengewindow = document.createElement('button');
+var noclick = document.createElement("div")
 
 
 //Get Data from JSON file and make it digestible
@@ -22,17 +22,6 @@ async function getDatafromJSON() {
 };
 getDatafromJSON()
 console.log(challenges)
-// Reload page and reset game
-
-resetbtn.addEventListener("click", function(){
-    window.location.reload();
-});
-
-function delay(milliseconds){
-    return new Promise(resolve => {
-        setTimeout(resolve, milliseconds);
-    });
-}
 
 //Get Number of Players and create NameInputFields
 
@@ -42,16 +31,13 @@ playerbtn.addEventListener("click", function(){
     section.innerHTML = '';
     for (let i = 0; i < nplayers; i++) {
         let inputfield = document.createElement("input");
-        let playersection = document.createElement("div");
-        playersection.id = `sec_player_${i+1}`;
         inputfield.id = `inp_player_${i+1}`;
         inputfield.type = 'text';
         inputfield.className = "inputfield";
         inputfield.placeholder = `Player ${i+1} name`;
         playerfieldids.push(inputfield.id);
-        sectionids.push(playersection.id);
-        section.appendChild(playersection);
-        playersection.appendChild(inputfield);
+        //sectionids.push(playersection.id);
+        section.appendChild(inputfield);
     };
     savebtn.id = "saveplayer_button";
     savebtn.textContent = "Save Players";
@@ -65,12 +51,12 @@ savebtn.addEventListener("click", function(){
     for (let i = 0, max = playerfieldids.length; i < max; i++) {
         playername = document.getElementById(playerfieldids[i]).value;
         playerfieldelement = document.getElementById(playerfieldids[i]);
-        sectionparent = document.getElementById(sectionids[i]);
+        //sectionparent = document.getElementById(playerfieldids[i]);
         players.push(playername);
         let newItem = document.createElement('div');
         newItem.innerHTML = playername;
         newItem.className = 'playername'
-        sectionparent.replaceChild(newItem, playerfieldelement);
+        section.replaceChild(newItem, playerfieldelement);
     }
 startbtn.id = "start_button";
 startbtn.textContent = "Start Game";
@@ -97,8 +83,8 @@ where n = number of divs,
 var Position = {
     ellipse : function(n, rx, ry, so, wh, idd, cls, cw) {
       var m = document.createElement('div'), ss = document.styleSheets;
-      ss[0].insertRule('#' + idd + ' { position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); border-radius: 50%; width: ' + String((rx * 2) + wh) + 'px; height: ' + String((ry * 2) + wh) + 'px; }', 1);
-      ss[0].insertRule('.' + cls + '{ position: absolute; color: #ffffff; text-align: center; transition: transform 0.2s ease; width: fit-content; height: fit-content; background: #1E1E1E; border: 2px solid #282828; box-shadow: 0px 0px 8px 1px #3E3E3E, inset 0px 0px 7px 8px rgba(0, 0, 0, 0.25); border-radius: 10%;  padding: 1% 2% 1% 2%; display: inline-block}', 1);
+      ss[0].insertRule('#' + idd + ' { position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); border-radius: 50%; width: ' + String((rx * 2) + wh) + 'px; height: ' + String((ry * 2) + wh) + 'px; display: none}', 1);
+      ss[0].insertRule('.' + cls + '{ position: absolute; color: #ffffff; text-align: center; transition: transform 0.2s ease; width: fit-content; height: fit-content; background: #1E1E1E; border: 2px solid #282828; box-shadow: 0px 0px 8px 1px #3E3E3E, inset 0px 0px 7px 8px rgba(0, 0, 0, 0.25); border-radius: 10%;  padding: 1% 2% 1% 2%; display: none}', 1);
       m.id = idd;
       for (var i = 0, max = players.length; i < max; i++) {
         var c = document.createElement('div');
@@ -115,7 +101,7 @@ var Position = {
         m.appendChild(c);
         
       };
-      document.body.appendChild(m);
+      circle.appendChild(m);
       
     }
   };
@@ -124,12 +110,12 @@ var Position = {
 
 startbtn.addEventListener("click", function(){
     document.body.innerHTML = '';
-    let circle = document.createElement('div');
+    var circle = document.createElement('div');
     circle.className = "circle";
     circle.id = "circle"
     circle.setAttribute("onclick", 'spin("Spinning...")');
     document.body.appendChild(circle);
-    Position.ellipse(players.length, 200, 200, 0, 50, 'playercircle', 'player', true);
+    Position.ellipse(players.length, 250, 250, 0, 50, 'playercircle', 'player', true);
     spinner.src = "arrow.png";
     spinner.className = "spinner";
     spinner.id = "arrow_png";
@@ -153,18 +139,21 @@ function spin(s){
 
 
 function displayChallange()
-{
-    challengewindow.id = "challenge";
+{   let task = challenges[Math.floor(Math.random()*challenges.length)];
+    challengewindow.id = 'challenge_' + task.id;
     challengewindow.className = "challenge";
-    let task = challenges[Math.floor(Math.random()*challenges.length)];
-    challengewindow.innerHTML = '<p id="challengepar" class="challengepar">' + task.challenge + '</p>';
+    challengewindow.innerHTML = '<div id="challengepar" class="challengepar">' + task.challenge + '</div>';
     document.body.appendChild(challengewindow);
     closechallengewindow.id = "close_challenge";
     closechallengewindow.textContent = "Close";
     closechallengewindow.className = "button";
     closechallengewindow.type = "button";
     challengewindow.appendChild(closechallengewindow);
+    noclick.className = "noclick";
+    noclick.id = "noclick";
+    document.body.appendChild(noclick);
     closechallengewindow.addEventListener("click", function(){
         document.body.removeChild(challengewindow);
+        document.body.removeChild(noclick)
     });
 }
